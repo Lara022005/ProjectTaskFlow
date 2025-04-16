0e723f6a848f975cea2c6643fb576bd7d7f63fee
sistemanoturno <sistemanoturno@LAB5-04.sistemafieto.rede>
2025-04-15 21:00:35 -0300

    alteração na controllerAgendamento

--- ProjectTaskFlow_/src/Controller/ControllerAgendamento.java
+++ ProjectTaskFlow_/src/Controller/ControllerAgendamento.java
@@ -1,5 +1,49 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ControllerAgendamento {
+
+    @FXML
+    private Button btAgendar;
+
+    @FXML
+    private Button btAlterar;
+
+    @FXML
+    private Button btConcluir;
+
+    @FXML
+    private Button btPesquisar;
+
+    @FXML
+    private TableView<?> tableAgendamentos;
+
+    @FXML
+    private TextField txtPesquisar;
+
+    @FXML
+    void actionAgendar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionAlterar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionConcluir(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionPesquisar(ActionEvent event) {
+
+    }
 
 }
