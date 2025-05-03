a943205402f47d632a03f44fc31fcd59bc27ca53
sistemanoturno <sistemanoturno@LAB5-04.sistemafieto.rede>
2025-05-02 21:01:06 -0300

    criação da controllerProduto

---
+++ ProjectTaskFlow_/src/Controller/ControllerProduto.java
@@ -0,0 +1,71 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ControllerProduto {
+
+    @FXML
+    private Button btCadastrar;
+
+    @FXML
+    private Button btCadastrar1;
+
+    @FXML
+    private Button btCliente;
+
+    @FXML
+    private Button btExcluir;
+
+    @FXML
+    private Button btExcluir1;
+
+    @FXML
+    private Button btFuncionario;
+
+    @FXML
+    private Button btMain;
+
+    @FXML
+    private Button btProduto;
+
+    @FXML
+    private Button btSair;
+
+    @FXML
+    void ActionCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ActionFuncionario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ActionMain(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ActionProduto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ActionSair(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionCadastrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionExcluir(ActionEvent event) {
+
+    }
+
+}
