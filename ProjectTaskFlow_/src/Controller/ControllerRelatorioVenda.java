24a8b3753f91b692a78895f738e925f48fd134d1
sistemanoturno <sistemanoturno@LAB5-04.sistemafieto.rede>
2025-05-21 19:37:15 -0300

    alterações servico e venda

--- ProjectTaskFlow_/src/Controller/ControllerRelatorioVenda.java
+++ ProjectTaskFlow_/src/Controller/ControllerRelatorioVenda.java
@@ -84,6 +84,7 @@ public class ControllerRelatorioVenda implements Initializable {
 		venda.setIdCliente(cliente.getNome());
 		ArrayList<String> nome = clienteDAO.readClienteByNome();
 	
+		
 		columnIndice.setCellValueFactory(new PropertyValueFactory<>("id"));
 		columnNomeCliente.setCellValueFactory(new PropertyValueFactory<>(nome + "idCliente"));
 		columnCpfCliente.setCellValueFactory(new PropertyValueFactory<>("idCliente"));
