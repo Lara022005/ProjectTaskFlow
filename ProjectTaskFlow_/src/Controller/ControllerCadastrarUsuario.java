f143f62f96d7227f834d9ec545e1dda121da2536
sistemanoturno <sistemanoturno@LAB5-09.sistemafieto.rede>
2025-05-21 18:54:15 -0300

    Tela funcionario

--- ProjectTaskFlow_/src/Controller/ControllerCadastrarUsuario.java
+++ ProjectTaskFlow_/src/Controller/ControllerCadastrarUsuario.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-
-import org.controlsfx.control.textfield.TextFields;
-
 import DAO.FuncionarioDAO;
 import DAO.UsuarioDAO;
 import Model.Funcionario;
@@ -21,117 +18,131 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class ControllerCadastrarUsuario implements Initializable {
 
-    @FXML
-    private AnchorPane TelaUsuario;
+	@FXML
+	private AnchorPane TelaUsuario;
 
-    @FXML
-    private Button btCadastrar;
+	@FXML
+	private Button btCadastrar;
 
-    @FXML
-    private Button btCliente;
+	@FXML
+	private Button btCliente;
 
-    @FXML
-    private Button btCancelar;
+	@FXML
+	private Button btCancelar;
 
-    @FXML
-    private Button btFuncionario;
+	@FXML
+	private Button btFuncionario;
 
-    @FXML
-    private Button btMain;
+	@FXML
+	private Button btMain;
 
-    @FXML
-    private Button btProduto;
+	@FXML
+	private Button btProduto;
 
-    @FXML
-    private Button btSair;
+	@FXML
+	private Button btSair;
 
-    @FXML
-    private TextField txtCpf;
+	@FXML
+	private TextField txtCpf;
 
-    @FXML
-    private TextField txtNivel;
+	@FXML
+	private TextField txtNivel;
 
-    @FXML
-    private TextField txtNome;
+	@FXML
+	private TextField txtNome;
 
-    @FXML
-    private TextField txtSenha;
+	@FXML
+	private TextField txtSenha;
 
-    @FXML
-    void ActionCliente(ActionEvent event) {
+	@FXML
+	void ActionCliente(ActionEvent event) throws IOException {
+		Main.TelaCliente();
+	}
 
-    }
+	@FXML
+	void ActionFuncionario(ActionEvent event) throws IOException {
+		Main.TelaFuncionario();
+	}
 
-    @FXML
-    void ActionFuncionario(ActionEvent event) {
+	@FXML
+	void ActionMain(ActionEvent event) throws IOException {
+		Main.TelaHome();
+	}
 
-    }
+	@FXML
+	void ActionProduto(ActionEvent event) throws IOException {
+		Main.TelaProduto();
+	}
 
-    @FXML
-    void ActionMain(ActionEvent event) {
+	@FXML
+	void ActionSair(ActionEvent event) throws IOException {
+		Main.TelaHome();
+	}
 
-    }
+	@FXML
+	void actionCadastrar(ActionEvent event) {
 
-    @FXML
-    void ActionProduto(ActionEvent event) {
 
-    }
-
-    @FXML
-    void ActionSair(ActionEvent event) throws IOException {
-    	Main.TelaHome();
-    }
-
-    @FXML
-    void actionCadastrar(ActionEvent event) {
-    	UsuarioDAO usuarioDAO = new UsuarioDAO();
-		Usuario usuario = new Usuario();
-		FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
-		Funcionario funcionario = new Funcionario();
-
-		ArrayList<Usuario> usuarios = new ArrayList<>();
-		usuario.setSenha(txtSenha.getText());
-		usuario.setNome(txtNome.getText());
-		usuario.setNivelUsuario(txtNivel.getText());
-		usuarios = usuarioDAO.search(usuario);
-		usuario = usuarios.get(0);
-
-		ArrayList<Funcionario> funcionarios = new ArrayList<>();
-		funcionario.setCpf(txtCpf.getText());
-		funcionarios = funcionarioDAO.search(funcionario);
-		funcionario = funcionarios.get(0);
-
-		
 		if(txtNome.getText() == "" || txtSenha.getText() == "" || txtNivel.getText() == "" || txtCpf.getText() == "") {   		
-			
-			Alerts.showAlert("Erro!", "Informações obrigatorias não foram preenchidas"," Verifique e tente novamente", AlertType.ERROR);    		
-		}else if(txtCpf.getText() == "" || cpfValidador.validarCPF(txtCpf.getText()) == false) {   		
+
+			Alerts.showAlert("Erro!", "Informações obrigatorias não foram preenchidas"," Verifique e tente novamente", AlertType.ERROR);   
+
+		}else if(txtCpf.getText() == "" || cpfValidador.validarCPF(txtCpf.getText()) == false) {   	
+
 			Alerts.showAlert("Erro!", "CPF inválido"," Verifique o CPF e tente novamente", AlertType.ERROR);
 		}
 		else {
-			usuario.setId(usuario.getId());
-			usuario.setNome(txtNome.getText());
+
+
+
+			UsuarioDAO usuarioDAO = new UsuarioDAO();
+			Usuario usuario = new Usuario();
+			FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
+			Funcionario funcionario = new Funcionario();
+
 			usuario.setSenha(txtSenha.getText());
+			usuario.setNome(txtNome.getText());
 			usuario.setNivelUsuario(txtNivel.getText());
-			usuario.setIdFuncionario(txtCpf.getText());
+
+
+			ArrayList<Funcionario> funcionarios = new ArrayList<>();
+			funcionario.setCpf(txtCpf.getText());
+			funcionarios = funcionarioDAO.search(funcionario);
+			funcionario = funcionarios.get(0);
+			usuario.setIdFuncionario(funcionario.getId());
+
+
+			usuarioDAO.create(usuario);
+
+
+			Alerts.showAlert("Sucesso!", "Usuario Cadastrado", "Usuario Cadastrado com sucesso", AlertType.INFORMATION);
+			//   		Stage stage = (Stage) btCancelar.getScene().getWindow();
+			//       	stage.close();
+
 
 		}
-    }
+	}
 
-    @FXML
-    void actionCancelar(ActionEvent event) {
-
-    }
+	@FXML
+	void actionCancelar(ActionEvent event) {
+		txtCpf.setText("");
+		txtNome.setText("");
+		txtSenha.setText("");
+		txtNivel.setText("");
+		Stage stage = (Stage) btCancelar.getScene().getWindow();
+		stage.close();
+	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		
-		
-		
+
+
+
 	}
 
 }
